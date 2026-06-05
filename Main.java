bf225518b1b11f24d6dc96bb2487f0a998475fe7
Zara1206 <2683590@dundee.ac.uk>
2026-06-05 11:37:22 +0100

    Add JPanel and buttons for Tic Tac Toe game layout

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import javax.swing.*;
 
 public class Main {
@@ -7,5 +8,17 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 400);
         frame.setVisible(true);
+
+    JPanel panel = new JPanel();
+    panel.setLayout(new GridLayout(3, 3));
+
+    JButton[] buttons = new JButton[9];
+    for (int i = 0; i < 9; i++) {
+        buttons[i] = new JButton("");
+        panel.add(buttons[i]);  
+    }
+
+    frame.add(panel);
+    frame.setVisible(true);
     }
 }
